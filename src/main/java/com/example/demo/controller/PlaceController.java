567bc3efc917f6e1daca06973e4bcc6fae6207a4
pooh <purinat.wan@dome.tu.ac.th>
2025-10-08 00:41:26 +0700

    add Task2.3-2.5(ADMIN)

--- src/main/java/com/example/demo/controller/PlaceController.java
+++ src/main/java/com/example/demo/controller/PlaceController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.model.Place;
 import com.example.demo.service.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,5 +25,33 @@ public class PlaceController {
     @GetMapping("/api/places/search")
     public List<Place> searchPlaces(@RequestParam String query) {
         return service.searchPlaces(query);
+    }
+    
+    //api  เพิ่ม
+    @SuppressWarnings("null")
+	@PostMapping
+    public ResponseEntity<Place> addPlace(@RequestBody Place place) {
+        PlaceService placeService = null;
+		Place newPlace = placeService.addPlace(place);
+        return ResponseEntity.ok(newPlace);
+    }
+    
+    //api แก้ไข
+    @PutMapping("/{id}")
+    public ResponseEntity<Place> updatePlace(@PathVariable Long id, @RequestBody Place place) {
+        PlaceService placeService = null;
+		@SuppressWarnings("null")
+		Place updated = placeService.updatePlace(id, place);
+        return ResponseEntity.ok(updated);
+    }
+    
+   
+    //api ลบ
+    @SuppressWarnings("null")
+	@DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePlace(@PathVariable Long id) {
+        PlaceService placeService = null;
+		placeService.deletePlace(id);
+        return ResponseEntity.ok("Place deleted successfully");
     }
 }
